c56ba6c9848b45c51d5c3a74578d93b3140ca55a
Nicoperez19 <nperez@ing.ucsc.cl>
2025-04-30 07:00:39 -0400

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,28 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
-    content: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
-    ],
-    theme: {
-      extend: {},
+  darkMode: 'class',
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './resources/**/*.ts',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1D4ED8',
+        secondary: '#9333EA',
+      },
+      fontFamily: {
+        sans: ['var(--default-font-family)', 'Inter', 'sans-serif'],
+      },
+      spacing: {
+        128: '32rem',
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,26 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        sourcemap: false,
+        chunkSizeWarningLimit: 500,
+    },
+    optimizeDeps: {
+        include: ['@tailwindcss/forms'],
+    },
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
 });
