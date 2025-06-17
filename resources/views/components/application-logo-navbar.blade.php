0190be64e326aa9dfdd32aa34847641751a6befa
Nicoperez19 <nperez@ing.ucsc.cl>
2025-06-17 01:31:44 -0400

    cambios logo

--- resources/views/components/application-logo-navbar.blade.php
+++ resources/views/components/application-logo-navbar.blade.php
@@ -1,7 +1,7 @@
 <img 
-    src="{{ asset('images/logo_active.png') }}" 
+    src="{{ asset('images/logo_IT_talcahuano.png') }}" 
     alt="Logo"
-    class="block object-contain w-24 my-1 ml-0 sm:w-32 md:w-36 lg:w-32 xl:w-58"
+    class="block object-contain w-24 my-1 ml-0 sm:w-32 md:w-44 lg:w-44 xl:w-64"
 />
 
     <!-- Aquí debe cambiar el tamaño según el tipo de pantalla: w-16 (móvil), w-20 (tablet), w-24 (ordenador medio), w-32 (pantallas grandes) -->
--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,7 +1,7 @@
 <img 
-    src="{{ asset('images/logo_active.png') }}" 
+    src="{{ asset('images/logo_IT_talcahuano02.png') }}" 
     alt="Logo"
-    class="block object-contain w-24 my-1 ml-0 sm:w-32 md:w-36 lg:w-60 xl:w-58"
+    class="block object-contain w-24 ml-0 sm:w-32 md:w-64 lg:w-64 xl:w-74"
 />
 
     <!-- Aquí debe cambiar el tamaño según el tipo de pantalla: w-16 (móvil), w-20 (tablet), w-24 (ordenador medio), w-32 (pantallas grandes) -->
