9ede3725d730f0920d25c57f646efc98f22f3d1d
Lucas Ayala Casanova <luayalac@outlook.cl>
2025-10-29 11:31:24 -0300

    fix salas

--- resources/views/reportes/accesos.blade.php
+++ resources/views/reportes/accesos.blade.php
@@ -286,20 +286,20 @@
                                     </span>
                                 </td>
                                 <td class="px-2 py-2">
-                                    <span class="inline-flex px-1 py-0.5 text-xs font-semibold rounded-full 
+                                    <span class="inline-flex px-2 py-0.5 text-xs font-semibold rounded-full whitespace-nowrap
                                         {{ $acceso['tipo_usuario'] == 'profesor' ? 'bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200' :
                                            ($acceso['tipo_usuario'] == 'estudiante' ? 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200' :
                                            ($acceso['tipo_usuario'] == 'administrativo' ? 'bg-purple-100 text-purple-800 dark:bg-purple-900 dark:text-purple-200' :
                                             'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200')) }}">
-                                        {{ substr(ucfirst($acceso['tipo_usuario']), 0, 4) }}
+                                        {{ ucfirst($acceso['tipo_usuario']) }}
                                     </span>
                                 </td>
                                 <td class="px-2 py-2">
-                                    <span class="inline-flex px-1 py-0.5 text-xs font-semibold rounded-full 
+                                    <span class="inline-flex px-2 py-0.5 text-xs font-semibold rounded-full whitespace-nowrap
                                         {{ $acceso['estado'] == 'activa' ? 'bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200' :
                                            ($acceso['estado'] == 'finalizada' ? 'bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-200' :
                                             'bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200') }}">
-                                        {{ substr(ucfirst($acceso['estado']), 0, 4) }}
+                                        {{ ucfirst($acceso['estado']) }}
                                     </span>
                                 </td>
                             </tr>
