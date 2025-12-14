aa60809202a4710cc1cc614257e01cc97c96a795
Lucas Ayala Casanova <luayalac@outlook.cl>
2025-12-14 18:06:17 -0300

    fix a los gráficos del dashboard

--- resources/views/layouts/professor/professor_index.blade.php
+++ resources/views/layouts/professor/professor_index.blade.php
@@ -45,7 +45,7 @@
             
             <!-- Ausencias de Profesores -->
             @can('gestionar licencias profesores')
-            <a href="{{ route('licencias.index') }}"
+            <a href="{{ route('ausencias-profesores') }}"
                class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-white transition-colors rounded-lg hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
                style="background-color: #cd1627;">
                 <i class="fa-solid fa-calendar-xmark"></i>
