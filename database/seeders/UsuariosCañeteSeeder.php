3e1fd01058de9e9c52220b6d4b4d757f4d4c34db
Lucas Ayala Casanova <luayalac@outlook.cl>
2026-01-15 10:41:02 -0300

    seeder cañete

---
+++ database/seeders/UsuariosCañeteSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsuariosCañeteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * Crea usuarios administrativos y de apoyo para la sede de Cañete
+     */
+    public function run(): void
+    {
+        // Directora de Sede Cañete
+        User::updateOrCreate(
+            ['run' => '11774643'],
+            [
+                'name' => 'Ana María Muñoz Fierro',
+                'email' => 'ammunoz@ucss.cl',
+                'password' => bcrypt('11774643'),
+                'celular' => '',
+                'direccion' => 'Avenida Presidente Frei',
+                'fecha_nacimiento' => null,
+            ]
+        )->assignRole('Supervisor');
+
+        // Encargado de Informática sede Cañete
+        User::updateOrCreate(
+            ['run' => '13581331'],
+            [
+                'name' => 'Benedicto Garrido Neculqueo',
+                'email' => 'bgarrido@ucss.cl',
+                'password' => bcrypt('13581331'),
+                'celular' => '',
+                'direccion' => 'Avenida Presidente Frei',
+                'fecha_nacimiento' => null,
+            ]
+        )->assignRole('Supervisor');
+
+        // Asistente Administrativa
+        User::updateOrCreate(
+            ['run' => '15200747'],
+            [
+                'name' => 'Nelly Verónica Santibaáez San Martín',
+                'email' => 'nsantibanez@ucss.cl',
+                'password' => bcrypt('15200747'),
+                'celular' => '',
+                'direccion' => 'Avenida Presidente Frei',
+                'fecha_nacimiento' => null,
+            ]
+        )->assignRole('Supervisor');
+    }
+}
