390999928ed5310c6cca471f507a238b617c9630
Nicoperez19 <89859977+Nicoperez19@users.noreply.github.com>
2026-09-05 00:55:51 -0400

    Correcion lenguaje

--- lang/es/pagination.php
+++ lang/es/pagination.php
@@ -1,10 +1,6 @@
-﻿<?php
+<?php
 
 return [
     'previous' => '&laquo; Anterior',
     'next' => 'Siguiente &raquo;',
-    'Showing' => 'Mostrando',
-    'to' => 'a',
-    'of' => 'de',
-    'results' => 'resultados',
 ];
